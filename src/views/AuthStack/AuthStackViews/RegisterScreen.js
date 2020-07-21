8aaf8438dc6800b8c52e76354585a15caa8c58a0
Nura <md.jahid@klientotech.com>
2020-07-21 15:40:02 +0530

    hello

---
+++ src/views/AuthStack/AuthStackViews/RegisterScreen.js
@@ -0,0 +1,123 @@
+import * as React from 'react';
+
+import {
+    View, Text, TouchableOpacity, StyleSheet,
+    ScrollView,
+    ImageBackground,
+    Image,
+    StatusBar,
+    ActivityIndicator,
+    Modal,
+    Dimensions,
+    TextInput,
+    ToastAndroid
+} from 'react-native';
+import { connect } from 'react-redux';
+import { Button, Icon, Item, Input, CheckBox, ListItem, Body } from 'native-base';
+
+// import { changeAuthState, changeProtocolState, changeToLogoutState } from '../../actions/authAction';
+import { getDimen } from '../../../dimensions/dimen';
+function RegisterScreen({ navigation }) {
+
+
+
+    const [checked, setChecked] = React.useState(false);
+    const [password, setPassword] = React.useState('');
+    const [username, setUsername] = React.useState('');
+    return (
+
+        <View>
+            <Text>
+                hello
+            </Text>
+            <Text>
+                hello
+            </Text>
+            <Text>
+                hello
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        top: 2,
+        width: '100%',
+        height: '100%'
+    },
+    input: {
+        height: 50,
+        flex: 10,
+        paddingLeft: 10,
+        fontSize: getDimen(0.045),
+        // fontFamily: 'Poppins-Regular',
+        color: 'black'
+    },
+    ImageStyle: {
+        padding: 10,
+        margin: 5,
+        height: 20,
+        width: 20,
+        resizeMode: 'stretch',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center'
+    },
+    inputContainerBottom: {
+        borderWidth: 1,
+        paddingBottom: 5,
+        borderBottomColor: '#CCC',
+        borderColor: 'transparent',
+        flexDirection: 'row',
+        marginTop: getDimen(.085),
+        marginLeft: getDimen(.085),
+        marginRight: getDimen(.085)
+    },
+    forgotContainer: {
+        paddingTop: 10,
+        paddingRight: 20,
+    },
+    forgotText: {
+        fontSize: 13,
+        alignSelf: 'flex-end',
+        color: '#F26622',
+    },
+    textStyle1: {
+        color: 'black',
+        alignSelf: 'flex-start',
+        marginTop: getDimen(.2),
+        marginLeft: getDimen(.085),
+        fontSize: getDimen(.045)
+    },
+    textStyle2: {
+        color: '#0088DD',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        marginTop: getDimen(.1),
+        // marginLeft: getDimen(.085),
+        fontSize: getDimen(.08)
+    },
+    textStyle3: {
+        color: 'black',
+        alignSelf: 'flex-start',
+        fontStyle: 'italic',
+        marginTop: getDimen(.085),
+        marginLeft: getDimen(.085),
+        fontSize: getDimen(.045)
+    },
+    body: {
+        backgroundColor: 'white',
+        // alignItems: "center", 
+        // justifyContent: "center",
+    },
+    itemStyle: {
+        marginTop: getDimen(.085),
+        marginLeft: getDimen(.085),
+        marginRight: getDimen(.085)
+    },
+});
+// const Login = connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
+export default RegisterScreen;
