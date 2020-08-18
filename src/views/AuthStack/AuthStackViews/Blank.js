4bcd321265ad403e529f1745f003d69ee6c131e9
Bharat Dixit <bharat.dixit@klientotech.com>
2020-08-18 17:53:30 +0530

    authentication flow

--- src/views/AuthStack/AuthStackViews/Blank.js
+++ src/views/AuthStack/AuthStackViews/Blank.js
@@ -33,14 +33,14 @@ function Blank({ navigation }) {
 
     
     return (
-        <View style={{ width: '100%', height: '100%' }}>
-            <ImageBackground
+        <View style={{ width: '100%', height: '100%', backgroundColor: 'white' }}>
+            {/* <ImageBackground
                 source={require('../../../assets/images/Splash.png')}
-                style={{ width: '100%', height: '100%', position: 'absolute' }}>
+                style={{ width: '100%', height: '100%', position: 'absolute', backgroundColor: 'white' }}>
 
                 
 
-            </ImageBackground>
+            </ImageBackground> */}
         </View>
 
     );
