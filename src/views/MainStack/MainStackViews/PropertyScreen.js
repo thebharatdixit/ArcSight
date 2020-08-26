c36b95e688d5e9c1628ca2f8bdeca2aa917afda1
Bharat Dixit <bharat.dixit@klientotech.com>
2020-08-26 15:43:30 +0530

    commit

--- src/views/MainStack/MainStackViews/PropertyScreen.js
+++ src/views/MainStack/MainStackViews/PropertyScreen.js
@@ -104,8 +104,8 @@ function PropertyScreen({ navigation }) {
         formData.append('price_per_sq_feet', 25);
         formData.append('price', 2500);
         formData.append('taxes', 25);
-        formData.append('amenities[]', 9);
-        formData.append('amenities[]', 2);
+        formData.append('amenities[]', arrSelectedAminitiesForApi);
+        // formData.append('amenities[]', 2);
         formData.append('description', 'Success');
         formData.append('is_featured', 'yes');
         formData.append('main_image', '');
